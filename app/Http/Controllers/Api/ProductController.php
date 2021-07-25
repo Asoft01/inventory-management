89905297a89ae38f1820f567885c01129a6298d4
Adeleke Hammed <lekhad19@gmail.com>
2021-07-25 21:51:43 +0100

    Project Product CURD Part 4

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Model\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Image;
 
 class ProductController extends Controller
 {
@@ -40,8 +42,45 @@ class ProductController extends Controller
             'buying_price' => 'required',
             'selling_price' => 'required',
             'buying_date' => 'required',
-            'category_id' => 'required'
+            'product_quantity' => 'required'
         ]);
+
+        if($request->image){
+            $position = strpos($request->image, ';');
+            $sub = substr($request->image, 0, $position);
+            $ext = explode('/', $sub)[1];
+
+            $name = time().".".$ext;
+            $img = Image::make($request->image)->resize(240, 200);
+            $upload_path = 'backend/product/';
+            $image_url = $upload_path.$name;
+            $img->save($image_url);
+
+            $product = new Product;
+            $product->category_id = $request->category_id;
+            $product->product_name = $request->product_name;
+            $product->product_code = $request->product_code;
+            $product->root = $request->root;
+            $product->buying_price = $request->buying_price;
+            $product->selling_price = $request->selling_price;
+            $product->supplier_id = $request->supplier_id;
+            $product->buying_date = $request->buying_date;
+            $product->product_quantity = $request->product_quantity;
+            $product->image = $image_url;
+            $product->save();
+        }else{
+            $product = new Product;
+            $product->category_id = $request->category_id;
+            $product->product_name = $request->product_name;
+            $product->product_code = $request->product_code;
+            $product->root = $request->root;
+            $product->buying_price = $request->buying_price;
+            $product->selling_price = $request->selling_price;
+            $product->supplier_id = $request->supplier_id;
+            $product->buying_date = $request->buying_date;
+            $product->product_quantity = $request->product_quantity;
+            $product->save();
+        }
     }
 
     /**
